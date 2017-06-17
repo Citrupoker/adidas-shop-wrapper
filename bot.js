621183aba75e1f1aafac08aeb24e526e4757adca
Slava Knyazev <slava@knyz.org>
2017-06-17 00:11:25 -0400

    a

--- bot.js
+++ bot.js
@@ -39,13 +39,13 @@ if (search.found) {
     console.log("Already found")
     return
 }
-request('http://localhost:3000/api/search/' + encodeURIComponent(search.query), function (error, response, body) {
+request('http://adidas-api-public.knyz.org/api/search/' + encodeURIComponent(search.query), function (error, response, body) {
     var result = JSON.parse(body);
 
     if (result.length != 0) {
         console.log("Results for ", search.query)
         console.log("Adding to cart...")
-        request('http://localhost:3000/api/cart/add/' + encodeURIComponent(result.items[search.nth].link) + "/" + encodeURIComponent(search.size), function (error, response, body) {
+        request('http://adidas-api-public.knyz.org/api/cart/add/' + encodeURIComponent(result.items[search.nth].link) + "/" + encodeURIComponent(search.size), function (error, response, body) {
             console.log("Added to cart: https://shop.adidas.ae/en/checkout/onepage/")
         })
     } else {
