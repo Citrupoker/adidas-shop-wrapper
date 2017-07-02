fe338401157a6c4a4246400c5d7468e4d92d2cb6
evans enonchong <evansenonchong@hotmail.com>
2017-07-02 09:50:59 -0400

    added proxy rotation

---
+++ proxyrotation/index.js
@@ -0,0 +1,22 @@
+
+var proxy = {}
+proxy.addr = ''
+proxy.user = ''
+proxy.password = ''
+proxy.genUseragent = function () {
+
+}
+
+module.exports.getProxy = function () {
+  return proxy
+}
+
+module.exports.addProxy = function (name, ip, port, user, pass) {
+
+}
+
+module.exports.deleteProxy = function (name) {
+
+}
+
+
