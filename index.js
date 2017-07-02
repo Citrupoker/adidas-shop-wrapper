bb9f94372ed3ca243d5a6adbe365f8e8f3454ad1
evans enonchong <evansenonchong@hotmail.com>
2017-07-02 13:52:02 -0400

    added wrapper

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
+var express = require('express')
+var app = express()
 var wrapper = require('./wrapper')
 var isBusy = false
 var proxyrotation = require('./proxyrotation')
