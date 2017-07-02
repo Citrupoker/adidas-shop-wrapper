057075d05a71aa0c1e09092e63ded76052d82e37
evans enonchong <evansenonchong@hotmail.com>
2017-07-02 13:30:19 -0400

    added wrapper

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express')
 const app = express()
 var wrapper = require('./wrapper')
 var isBusy = false
-
+var proxyrotation = require('./proxyrotation')
+var accounts = require('./accounts')
 
 app.use('/api/*', (req, res, next) => {
   console.log('Hit API end point')
   res.setHeader('Content-Type', 'application/json')
   next()
 })
-
 
 function throttler (req, res, next) {
   if (isBusy) {
@@ -22,7 +22,6 @@ function throttler (req, res, next) {
   }
 }
 
-
 app.post('/api/cart/add/:url/:size', throttler, function (req, res) {
   wrapper.addToCart(req.body.url, req.body.size, req.body.account, () => {
     res.json({ status: 1 })
@@ -30,20 +29,16 @@ app.post('/api/cart/add/:url/:size', throttler, function (req, res) {
   })
 })
 
-
 app.get('/api/info/:item', function (req, res) {
   wrapper.itemInfo(req.params.item, (info) => {
     res.json(info)
   })
 })
 
-
 app.get('/', function (req, res) {
   res.setHeader('Content-Type', 'text/plain')
   res.sendFile(__dirname + '/README.md')
 })
-
-
 
 app.get('/api/search/:searchQuery', function (req, res) {
   wrapper.search(req.params.searchQuery, (results) => {
@@ -51,7 +46,40 @@ app.get('/api/search/:searchQuery', function (req, res) {
   })
 })
 
+app.post('/api/add/proxy', function (req, res) {
+  var proxy = req.body.addr
+  var port = req.body.port
+  var name = req.body.name
+  var user = req.body.user
+  var pass = req.body.pass
 
+  if (proxy && port && name && user && pass) {
+    proxyrotation.addProxy(name, proxy, port, user, pass)
+    return res.json({status: 1})
+  }
+  return res.json({status: 0})
+})
+
+app.get('/api/delete/proxy/:name', function (req, res) {
+  var name = req.params.name
+  if (name) {
+    proxyrotation.deleteProxy(name)
+    return res.json({status: 1})
+  }
+  return res.json({status: 0})
+})
+
+app.post('/api/add/account', function (req, res) {
+  var name = req.body.name
+  var email = req.body.email
+  var pass = req.body.pass
+  accounts.addAccount(name, email, pass)
+})
+
+app.get('/api/delete/account', function (req, res) {
+  var name = req.body.name
+  accounts.removeAccount(name)
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
